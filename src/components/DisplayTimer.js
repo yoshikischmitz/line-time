866d237d6f75d4ae69ca2281b9d9458f8ec109bd
yoshiki <yoshikischmitz@outlook.com>
2018-02-23 11:10:27 +0900

    update control group styling

--- src/components/DisplayTimer.js
+++ src/components/DisplayTimer.js
@@ -12,11 +12,22 @@ export default ({seconds, onStopClick, onStartClick, onSkipClick, state}) => {
 	const timerActive = (state === 'Playing' || state === 'Paused')
 	return (
 		<div className="control-group">
-			{ (timerActive) && <div className="timer-control stop" onClick={onStopClick}>Stop</div> }
+			{ (timerActive) && 
+					<div className="timer-control stop" onClick={onStopClick}>Stop
+						<div className="icon"></div>
+				  </div> 
+			}
 			<div className="timer-control play" onClick={onStartClick}>
 				{ stateMap[state] }
+						<div className="icon"></div>
 			</div>
-			{ (timerActive) && <div className="timer-control skip" onClick={onSkipClick} >Skip</div> }
+			{ 
+				(timerActive) && 
+					<div className="timer-control skip" onClick={onSkipClick} >
+						Skip
+						<div className="icon"></div>
+					</div> 
+			}
 		</div>
 	)
 }
