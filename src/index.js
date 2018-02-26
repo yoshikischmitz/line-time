1793255966ca233a7525d3cb58cb669e338b60a6
yoshiki <yoshikischmitz@outlook.com>
2018-02-26 17:23:00 +0900

    add throttling

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import lineTimeApp from './reducers'
 import App from './components/App'
 import {tick} from  './actions'
 import {serializeChunks} from './utils'
+import throttle from 'lodash/throttle'
 
 const composeEnhancers =
   typeof window === 'object' &&
@@ -20,11 +21,13 @@ const store = createStore(lineTimeApp, composeEnhancers(applyMiddleware(thunk)))
 
 const storage = window.localStorage
 
-store.subscribe(() => {
+const persist = throttle(() => {
 	const state = store.getState()
 	const readyForJson = {...state, chunks: serializeChunks(state.chunks)}
 	storage.setItem("line-time-state", JSON.stringify(readyForJson))
-})
+}, 3000)
+
+store.subscribe(persist)
 
 setInterval(() => { store.dispatch(tick())}, 1000)
 
